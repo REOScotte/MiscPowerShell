4ea6941e8ed43b7daf3d0d66c28aafe66ccde2b9
REOScotte <11577865+REOScotte@users.noreply.github.com>
2021-07-23 10:29:59 -0500

    Change endBlock variable prefix to something more readable

--- Invoke-RemoteTemplate.ps1
+++ Invoke-RemoteTemplate.ps1
@@ -58,7 +58,7 @@ Run on Comp1 over port 8080
 Invoke-RemoteTemplate -InvokeCommandParameters @{Port = 8080}
 
 .NOTES
-To avoid variable name collisions, the End block uses variable names with the _endBlock_ prefix. 
+To avoid variable name collisions, the End block uses variable names with the __ prefix. 
 
 Author: Scott Crawford
 Created: 2020-09-30
@@ -121,35 +121,35 @@ function Invoke-RemoteTemplate {
     end {
         #region Parse the Abtract Syntax Tree of the current script to pull out CmdletBinding attributes,parameters, and defined variables.
         # Get the entire AST of the function
-        $_endBlock_functionAst = (Get-Command $MyInvocation.MyCommand).ScriptBlock.Ast
+        $__functionAst = (Get-Command $MyInvocation.MyCommand).ScriptBlock.Ast
 
         # Get the CmdletBinding attribute of the param block
-        $_endBlock_predicate = {
+        $__predicate = {
             param ( [System.Management.Automation.Language.Ast] $AstObject )
             return ( $AstObject -is [System.Management.Automation.Language.AttributeAst] )
         }
-        $_endBlock_CmdletBindingAttribute = $_endBlock_functionAst.Body.ParamBlock.Attributes.FindAll($_endBlock_predicate, $false) |
+        $__CmdletBindingAttribute = $__functionAst.Body.ParamBlock.Attributes.FindAll($__predicate, $false) |
             Where-Object { $_.TypeName.FullName -eq 'CmdletBinding' } |
             Select-Object -ExpandProperty Extent |
             Select-Object -ExpandProperty Text
 
         # Find the parameters in the param block except for ComputerName, Session, and InvokeCommandParameters
-        $_endBlock_predicate = {
+        $__predicate = {
             param ( [System.Management.Automation.Language.Ast] $AstObject )
             return ( $AstObject -is [System.Management.Automation.Language.ParameterAst] )
         }
-        $_endBlock_Parameters = $_endBlock_functionAst.Body.ParamBlock.Parameters.FindAll($_endBlock_predicate, $false) |
+        $__Parameters = $__functionAst.Body.ParamBlock.Parameters.FindAll($__predicate, $false) |
             Where-Object { $_.Name.VariablePath.UserPath -notin @('ComputerName', 'Session', 'InvokeCommandParameters') } |
             Select-Object -ExpandProperty Name |
             Select-Object -ExpandProperty VariablePath |
             Select-Object -ExpandProperty UserPath
 
         # Find the variables assigned in the Begin block except for script and postScript
-        $_endBlock_predicate = {
+        $__predicate = {
             param ( [System.Management.Automation.Language.Ast] $AstObject )
             return ( $AstObject -is [System.Management.Automation.Language.AssignmentStatementAst] )
         }
-        $_endBlock_BeginVariables = $_endBlock_functionAst.Body.BeginBlock.Statements.FindAll($_endBlock_predicate, $false) |
+        $__BeginVariables = $__functionAst.Body.BeginBlock.Statements.FindAll($__predicate, $false) |
             Select-Object @{n = 'Name'; e = {
                     if ($_.Left.Child.VariablePath.UserPath) {
                         $_.Left.Child.VariablePath.UserPath
@@ -164,13 +164,13 @@ function Invoke-RemoteTemplate {
         
         #region Param Script
         # This scriptblock defines that param block for the function. It includes the CmdletBinding attribute found above.
-        # It also copies PSBoundParameters to another variable. This allows them to be visible when the script is running locally.
-        $_endBlock_PSBoundParameters = $PSBoundParameters
-        $_endBlock_ParamScript = "
-            $_endBlock_CmdletBindingAttribute
+        # It also copies PSBoundParameters to another variable. This allows is to be visible when the script is running locally.
+        $__PSBoundParameters = $PSBoundParameters
+        $__ParamScript = "
+            $__CmdletBindingAttribute
             param()
             
-            `$PSBoundParameters = `$_endBlock_PSBoundParameters
+            `$PSBoundParameters = `$__PSBoundParameters
         "
         #endregion
 
@@ -182,41 +182,43 @@ function Invoke-RemoteTemplate {
         # if ($condition) {$test = 'test'}
         # PSBoundParameters is also added to pick up DynamicParameters and to pass this info to remote machines.
 
-        $_endBlock_LocalVariableNames = @()
-        if ($_endBlock_Parameters)     { $_endBlock_LocalVariableNames += $_endBlock_Parameters     }
-        if ($_endBlock_BeginVariables) { $_endBlock_LocalVariableNames += $_endBlock_BeginVariables }
+        $__LocalVariableNames = @()
+        if ($__Parameters)     { $__LocalVariableNames += $__Parameters     }
+        if ($__BeginVariables) { $__LocalVariableNames += $__BeginVariables }
 
-        $_endBlock_VariableScript = "`ntry {`n"
-        foreach ($_endBlock_LocalVariableName in $_endBlock_LocalVariableNames) {
-            $_endBlock_LocalVariable = Get-Variable | Where-Object Name -EQ $_endBlock_LocalVariableName
-            if ($_endBlock_LocalVariable) {
-                $_endBlock_VariableValue = $_endBlock_LocalVariable.Value
-                if ($_endBlock_VariableValue) {
-                    $_endBlock_TypeName = $_endBlock_LocalVariable.Value.GetType().FullName
+        $__VariableScript = "`ntry {`n"
+        foreach ($__LocalVariableName in $__LocalVariableNames) {
+            $__LocalVariable = Get-Variable | Where-Object Name -EQ $__LocalVariableName
+            if ($__LocalVariable) {
+                $__VariableValue = $__LocalVariable.Value
+                if ($__VariableValue) {
+                    $__TypeName = $__LocalVariable.Value.GetType().FullName
 
-                    # Not all variable types cross the "using" threshold intact. Specific types can be called out here to correct for that.
-                    $_endBlock_UsingSide = switch ($_endBlock_TypeName) {
-                        'System.Management.Automation.ScriptBlock' { "[scriptblock]::Create(`$using:$_endBlock_LocalVariableName)" }
-                        'System.Security.AccessControl.DirectorySecurity' {
-                            $_endBlock_TempAclSddl = $_endBlock_LocalVariable.Value.Sddl
-                            "[System.Security.AccessControl.DirectorySecurity]::new();`$$_endBlock_LocalVariableName.SetSecurityDescriptorSddlForm(`$using:_endBlock_TempAclSddl)"
+                    # Not all variable types cross the "using" threshold seamlessly. Shims for specific types can be implemented here.
+                    $__UsingSide = switch ($__TypeName) {
+                        'System.Management.Automation.ScriptBlock' {
+                            "[scriptblock]::Create(`$using:$__LocalVariableName)"
                         }
-                        default { "`$using:$_endBlock_LocalVariableName" }
+                        'System.Security.AccessControl.DirectorySecurity' {
+                            $__TempAclSddl = $__LocalVariable.Value.Sddl
+                            "[System.Security.AccessControl.DirectorySecurity]::new();`$$__LocalVariableName.SetSecurityDescriptorSddlForm(`$using:__TempAclSddl)"
+                        }
+                        default { "`$using:$__LocalVariableName" }
                     }
                 } else {
-                    $_endBlock_UsingSide = '$null'
+                    $__UsingSide = '$null'
                 }
-                $_endBlock_VariableScript += "    `$$_endBlock_LocalVariableName = $_endBlock_UsingSide`n"
+                $__VariableScript += "    `$$__LocalVariableName = $__UsingSide`n"
             }
         }
-        $_endBlock_VariableScript += "    `$PSBoundParameters = `$using:_endBlock_PSBoundParameters`n"
-        $_endBlock_VariableScript += "} catch {}`n"
+        $__VariableScript += "    `$PSBoundParameters = `$using:__PSBoundParameters`n"
+        $__VariableScript += "} catch {}`n"
         #endregion
 
         #region Import preference variables
 
         # This scriptblock imports all the current preferences to ensure those settings are passed to $script.
-        $_endBlock_PreferenceScript = {
+        $__PreferenceScript = {
             try {
                 $ConfirmPreference     = $using:ConfirmPreference
                 $DebugPreference       = $using:DebugPreference
@@ -245,38 +247,38 @@ function Invoke-RemoteTemplate {
 
         #region Assemble the 4 scripts and run it
         # The 4 scripts - Param, Variable, Preference, and the main script are combined into a single scriptblock.
-        $_endBlock_TotalScript = [scriptblock]::Create($_endBlock_ParamScript + $_endBlock_VariableScript + $_endBlock_PreferenceScript.ToString() + $script.ToString())
+        $__TotalScript = [scriptblock]::Create($__ParamScript + $__VariableScript + $__PreferenceScript.ToString() + $script.ToString())
 
         # Run the total script on all applicable targets - computers, sessions, or local.
         switch ($PSCmdlet.ParameterSetName) {
             'Session' {
                 if ($allSessions) {
-                    Invoke-Command -Session $allSessions -ScriptBlock $_endBlock_TotalScript -ErrorAction SilentlyContinue -ErrorVariable +ErrorVar @InvokeCommandParameters
+                    Invoke-Command -Session $allSessions -ScriptBlock $__TotalScript -ErrorAction SilentlyContinue -ErrorVariable +__ErrorVar @InvokeCommandParameters
                 }
             }
             'Computer' {
                 if ($allComputers) {
-                    Invoke-Command -ComputerName $allComputers -ScriptBlock $_endBlock_TotalScript -ErrorAction SilentlyContinue -ErrorVariable +ErrorVar @InvokeCommandParameters
+                    Invoke-Command -ComputerName $allComputers -ScriptBlock $__TotalScript -ErrorAction SilentlyContinue -ErrorVariable +__ErrorVar @InvokeCommandParameters
                 }
             }
             'Local' {
-                & $_endBlock_TotalScript
+                & $__TotalScript
             }
         }
         #endregion
 
-        #region Report any errors collected in $ErrorVar
+        #region Report any errors collected in $__ErrorVar
         # If the error has an OriginInfo.PSComputerName, it occurred ON the remote computer so the remote error is written here as a warning.
         # If the error has a TargetObject.ComputerName, it occurred trying to connect to a session object so report the computer name.
         # If the error just has a TargetObject, it occured trying to connect to a remote computer, so report the computer name.
         # Otherwise, something strange is happening.
-        foreach ($record in $ErrorVar) {
-            if ($record.OriginInfo.PSComputerName) {
-                Write-Warning "Error: $($record.Exception.Message) $($record.OriginInfo.PSComputerName)"
-            } elseif ($record.TargetObject.ComputerName) {
-                Write-Warning "Unable to connect to $($record.TargetObject.ComputerName)"
-            } elseif ($record.TargetObject) {
-                Write-Warning "Unable to connect to $($record.TargetObject)"
+        foreach ($__record in $__ErrorVar) {
+            if ($__record.OriginInfo.PSComputerName) {
+                Write-Warning "Error: $($__record.Exception.Message) $($__record.OriginInfo.PSComputerName)"
+            } elseif ($__record.TargetObject.ComputerName) {
+                Write-Warning "Unable to connect to $($__record.TargetObject.ComputerName)"
+            } elseif ($__record.TargetObject) {
+                Write-Warning "Unable to connect to $($__record.TargetObject)"
             } else {
                 throw 'Unexpected error'
             }
